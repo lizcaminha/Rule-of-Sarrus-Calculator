e73ea58abc76a197b23977ec1e17a4357bebb3b7
lizcaminha <lizcaminha@gmail.com>
2025-07-02 20:24:36 -0400

    syntax and logic error fixes
    the numbers still arent adding up

--- javadia2.java
+++ javadia2.java
@@ -3,35 +3,42 @@ public class javadia2 {
     public static void main (String[] args){
         Random randint = new Random();
         int[][] matriza = new int[3][3];
-        int determinantePrincipal=1, determinanteSecundaria=1, contador=0, somaDiag1=0, somaDiag2 = 0, determinante = 0;
+        int determinantePrincipal=1, determinanteSecundaria=1, contador=0, somaDiag1=0, somaDiag2 = 0, determinante = 0, i, j, somaDiagonalPrincipal = 0, somaDiagonalSecundaria = 0;
         //população da matriz
-        for (int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        for (i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
                 matriza[i][j] = randint.nextInt(10);
             }
         }
         //soma das diagonais!
         for(i=0; i<3; i++){
-             int somaDiagonalPrincipal =+ matriza[i][i];
-             int somaDiagonalSecundaria =+ matriza[i][2-i];
+             somaDiagonalPrincipal =+ matriza[i][i];
+             somaDiagonalSecundaria =+ matriza[i][2-i];
         }
-        for(i=0; i<=9; i++){
-            determinantePrincipal=matriza[contador][i]*determinantePrincipal;
-            determinanteSecundaria = matriza[contador][2-i]*determinanteSecundaria;
-            if(contador%3==0){
-                contador=0+(contador/3);
-                somaDiag1 = determinantePrincipal + somaDiag1;
-                somaDiag2 = determinanteSecundaria - somaDiag2;
+        for(i=0; i<3; i++){
+            for(j=0; j<3; j++){
+                if(contador==3){
+                    if(j==0){
+                        contador=i;
+                    }else{
+                        contador=0;
+                    }
+                    somaDiag1 = determinantePrincipal + somaDiag1;
+                    somaDiag2 = determinanteSecundaria - somaDiag2;
+                }
+                determinantePrincipal=matriza[contador][j]*determinantePrincipal;
+                determinanteSecundaria = matriza[contador][2-j]*determinanteSecundaria;
+
+                contador++;
             }
-            contador++;
         }
         determinante = somaDiag1 - somaDiag2;
-         for (int i = 0; i < 3; i++){
-            System.out.println("\n");
-            for(int j = 0; j < 3; j++){
-                System.out.println(matriza[i][j] + " ");
+         for (i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                System.out.print(matriza[i][j] + " ");
             }
+            System.out.println("\n");
         }
-        System.out.println("Determinante: " + determinante);
+        System.out.println("Determinante: " + determinante +"\nSoma da Diagonal Principal: " + somaDiagonalPrincipal +"\nSoma da Diagonal Secundaria: " + somaDiagonalSecundaria);
     }
 }
